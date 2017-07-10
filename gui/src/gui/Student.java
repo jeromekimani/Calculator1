e02350360ab2309e0e90964bd22a44ad0221b765
jeromekimani <jkimani003@gmail.com>
2017-07-10 17:36:04 +0300

    Add files via upload

---
+++ gui/src/gui/Student.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author 100242
+ */
+public class Student {
+    private String firstName;
+    private String lastName;
+public Student(String first, String second){
+    firstName=first;
+    lastName=second;
+    
+}
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getStudentDetails(){
+    
+    return "Student is"+firstName+" "+lastName;
+    
+    }
+    
+    
+}
